a49f7413b01afff8ba78d3fcb792d5a5e4f76f18
Devin Mounts and Thad Donaghue <devin@>
2018-07-10 09:58:04 -0700

    create GetAll method for db.

---
+++ Oregon/Models/Database.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using Oregon;
+
+namespace Oregon.Models
+{
+    public class DB
+    {
+        public static MySqlConnection Connection()
+        {
+            MySqlConnection conne = new MySqlConnection(DBConfiguration.ConnectionString);
+            return conne;
+        }
+    }
+}
--- Oregon/Models/Places.cs
+++ Oregon/Models/Places.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 namespace Oregon.Models
 {
     public class Places
@@ -7,19 +8,26 @@ namespace Oregon.Models
         private static int _id = 0;
         private string _name;
         private string _category;
-        private DateTime _hours = new DateTime();
-        private Location _location;
+        private string _opening;
+        private string _closing;
+        //private Location _location;
+        private string _zip;
 
 
-        public Places(string name, string category, DateTime hours, Location location)
+        public Places(int id, string name, string category, string opening, string closing, string zip)
         {
-            _id++;
-            _name = name;
+            _id = id;
+            _name = name; 
             _category = category;
-            _hours = hours;
-            _location = location;
+            _opening = opening;
+            _closing = closing;
+            _zip = zip;
         }
 
+        public int GetId()
+        {
+            return _id;
+        }
         public string GetName()
         {
             return _name;
@@ -30,14 +38,58 @@ namespace Oregon.Models
             return _category;
         }
 
-        public DateTime GetHours()
+        public string GetOpening()
         {
-            return _hours;
+            return _opening;
         }
 
-        public Location GetLocation()
+        public string GetClosing()
         {
-            return _location;
+            return _closing;
+        }
+
+        public static void DeleteAll()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM places;";
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public static List<Places> GetAll()
+        {
+            List<Places> allPlaces = new List<Places> { new Places("testcafe", "cafe", "7", "7", "92331") };
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM places;";
+            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while(rdr.Read())
+            {
+                int Id = rdr.GetInt32(0);
+                string Name = rdr.GetString(1);
+                string Category = rdr.GetString(2);
+                string Opening = rdr.GetString(3);
+                string Closing = rdr.GetString(4);
+                string Zip = rdr.GetString(5);
+                Places newPlace = new Places(Id, Name, Category, Opening, Closing, Zip);
+                allPlaces.Add(newPlace);
+            }
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return allPlaces;
         }
     }
 }
