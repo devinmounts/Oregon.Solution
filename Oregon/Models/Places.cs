97b767726b95e71ccb1f6c626ed97cb1e27442d7
Devin Mounts and Thad Donaghue <thad@>
2018-07-10 11:37:42 -0700

    test save and getall to database.

--- Oregon/Models/Places.cs
+++ Oregon/Models/Places.cs
@@ -7,6 +7,7 @@ namespace Oregon.Models
     {
         private static int _id = 0;
         private string _name;
+        private string _address;
         private string _category;
         private string _opening;
         private string _closing;
@@ -14,10 +15,11 @@ namespace Oregon.Models
         private string _zip;
 
 
-        public Places(int id, string name, string category, string opening, string closing, string zip)
+        public Places(int id, string name, string address, string category, string opening, string closing, string zip)
         {
             _id = id;
-            _name = name; 
+            _name = name;
+            _address = address;
             _category = category;
             _opening = opening;
             _closing = closing;
@@ -33,6 +35,11 @@ namespace Oregon.Models
             return _name;
         }
 
+        public string GetAddress()
+        {
+            return _address;
+        }
+
         public string GetCategory()
         {
             return _category;
@@ -46,6 +53,72 @@ namespace Oregon.Models
         public string GetClosing()
         {
             return _closing;
+        }
+
+        public void Save() 
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"INSERT INTO `places` (`name`, `address`, `category`, `opening`, `closing`, `zip`) VALUES (@thisName, @thisAddress, @thisCategory, @thisOpening, @thisClosing, @thisZip);";
+
+            MySqlParameter name = new MySqlParameter();
+            name.ParameterName = "@thisName";
+            name.Value = this._name;
+            MySqlParameter address = new MySqlParameter();
+            address.ParameterName = "@thisAddress";
+            address.Value = this._address;
+            MySqlParameter category = new MySqlParameter();
+            category.ParameterName = "@thisCategory";
+            category.Value = this._category;
+            MySqlParameter opening = new MySqlParameter();
+            opening.ParameterName = "@thisOpening";
+            opening.Value = this._opening;
+            MySqlParameter closing = new MySqlParameter();
+            closing.ParameterName = "@thisClosing";
+            closing.Value = this._closing;
+            MySqlParameter zip = new MySqlParameter();
+            zip.ParameterName = "@thisZip";
+            zip.Value = this._zip;
+            cmd.Parameters.Add(name);
+            cmd.Parameters.Add(address);
+            cmd.Parameters.Add(category);
+            cmd.Parameters.Add(opening);
+            cmd.Parameters.Add(closing);
+            cmd.Parameters.Add(zip);
+
+            cmd.ExecuteNonQuery();
+            _id = (int) cmd.LastInsertedId;
+
+            conn.Close();
+            if(conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public override bool Equals(System.Object otherPlace)
+        {
+            //bool placeEquality = false;
+            //Places newPlace;
+
+            if (!(otherPlace is Places))
+            {
+                return false;
+            }
+            else
+            {
+                Places newPlace = (Places)otherPlace;
+                bool idEquality = (this.GetId() == newPlace.GetId());
+                bool addressEquality = (this.GetAddress() == newPlace.GetAddress());
+                return (idEquality && addressEquality);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.GetName().GetHashCode();
         }
 
         public static void DeleteAll()
@@ -67,21 +140,22 @@ namespace Oregon.Models
 
         public static List<Places> GetAll()
         {
-            List<Places> allPlaces = new List<Places> { new Places("testcafe", "cafe", "7", "7", "92331") };
+            List<Places> allPlaces = new List<Places> { };
             MySqlConnection conn = DB.Connection();
             conn.Open();
             MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
             cmd.CommandText = @"SELECT * FROM places;";
             MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
-            while(rdr.Read())
+            while (rdr.Read())
             {
                 int Id = rdr.GetInt32(0);
                 string Name = rdr.GetString(1);
-                string Category = rdr.GetString(2);
-                string Opening = rdr.GetString(3);
-                string Closing = rdr.GetString(4);
-                string Zip = rdr.GetString(5);
-                Places newPlace = new Places(Id, Name, Category, Opening, Closing, Zip);
+                string Address = rdr.GetString(2);
+                string Category = rdr.GetString(3);
+                string Opening = rdr.GetString(4);
+                string Closing = rdr.GetString(5);
+                string Zip = rdr.GetString(6);
+                Places newPlace = new Places(Id, Name, Address, Category, Opening, Closing, Zip);
                 allPlaces.Add(newPlace);
             }
             conn.Close();
--- OregonTests/ModelsTests/PlacesTests.cs
+++ OregonTests/ModelsTests/PlacesTests.cs
@@ -1,13 +1,64 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Oregon.Models;
+using System;
+using System.Collections.Generic;
 
-namespace OregonTests
+namespace Oregon.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class PlacesTests : IDisposable
     {
-        [TestMethod]
-        public void TestMethod1()
+        public void Dispose()
         {
+            Places.DeleteAll();
+        }
+
+        public PlacesTests()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=places;";
+        }
+
+        [TestMethod]
+        public void GetAll_DbStartsEmpty_0()
+        {
+            int result = Places.GetAll().Count;
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Equals_ReturnsTrueIfIDsAreTheSame_Place()
+        {
+            Places firstPlace = new Places(1, "Starbucks", "123 Fake St.", "Coffee Shop", "8", "8", "97202");
+            Places secondPlace = new Places(2, "Starbucks", "123 Fake St.", "Coffee Shop", "6", "7", "97219");
+
+            Assert.AreEqual(firstPlace, secondPlace);
+        }
+
+        [TestMethod]
+        public void Save_SavesToDatabase_ItemList()
+        {
+            Places testPlace = new Places(1, "Starbucks", "123 Fake St.", "Coffee Shop", "8", "8", "97202");
+
+            testPlace.Save();
+            List<Places> result = Places.GetAll();
+            List<Places> testList = new List<Places> { testPlace };
+
+            CollectionAssert.AreEqual(testList, result);
+        }
+
+        [TestMethod]
+        public void Save_AssignsIdToObject_Id()
+        {
+            Places testPlace = new Places(1, "Starbucks", "123 Fake St.", "Coffee Shop", "8", "8", "97202");
+
+            testPlace.Save();
+            Places savedPlace = Places.GetAll()[0];
+
+            int result = savedPlace.GetId();
+            int testId = testPlace.GetId();
+
+            Assert.AreEqual(testId, result);
         }
     }
 }
