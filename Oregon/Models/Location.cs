e9374804aeaa5ab2dd7c3975440ef3d296e3459f
Devin Mounts and Thad Donaghue <devin@>
2018-07-10 09:22:49 -0700

    initial commit.

---
+++ Oregon/Models/Location.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Oregon.Models
+{
+    public class Location
+    {
+        private string _address;
+        private string _state;
+        private string _city;
+        private string _zip;
+
+        public Location(string address, string state, string city, string zip)
+        {
+            _address = address;
+            _state = state;
+            _city = city;
+            _zip = zip;
+        }
+
+        public string GetAddress()
+        {
+            return _address;
+        }
+
+        public string GetState()
+        {
+            return _state;
+        }
+
+        public string GetCity()
+        {
+            return _city;
+        }
+
+        public string GetZip()
+        {
+            return _zip;
+        }
+    }
+}
---
+++ Oregon/Models/Places.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Oregon.Models
+{
+    public class Places
+    {
+        private static int _id = 0;
+        private string _name;
+        private string _category;
+        private DateTime _hours = new DateTime();
+        private Location _location;
+
+
+        public Places(string name, string category, DateTime hours, Location location)
+        {
+            _id++;
+            _name = name;
+            _category = category;
+            _hours = hours;
+            _location = location;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public string GetCategory()
+        {
+            return _category;
+        }
+
+        public DateTime GetHours()
+        {
+            return _hours;
+        }
+
+        public Location GetLocation()
+        {
+            return _location;
+        }
+    }
+}
